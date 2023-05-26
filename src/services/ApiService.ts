bf8ee9db37e89e915aca41c39e607d474f26969e
Rodolfo Correia <rodolfo.correia02@gmail.com>
2023-05-26 00:00:46 -0300

    Update ApiService.ts

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -7,17 +7,23 @@ export default class ApiService {
   async postLogin(loginForm: any) {
     const response = await axios.post(`${API_URL}/login`, loginForm);
     return response;
-  }
+  };
 
   async postLembretes(lembreteForm: any) {
     const response = await axios.post(`${API_URL}/lembretes`, lembreteForm, {headers: {Authorization: `Bearer ${access_token}`},
   });
     return response;
-  }
+  };
 
   async getAllLembretes(codigoUsuario: string) {
     const response = await axios.get(`${API_URL}/lembretes/${codigoUsuario}`, {headers: {Authorization: `Bearer ${access_token}`},
     });
     return response;
-  }
+  };
+
+  async deleteLembretes(codigoUsuario: string, codigoLembrete: number) {
+    const response = await axios.delete(`${API_URL}/lembretes/${codigoUsuario}/${codigoLembrete}`, {headers: {Authorization: `Bearer ${access_token}`},
+  });
+    return response;
+  };
 }
